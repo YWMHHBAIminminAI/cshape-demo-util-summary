3009db961742ae63456e17f31b7a0163f88126e0
AIminminAI <1317957931@qq.com>
2023-07-09 02:51:23 +0000

    定义一个桌面时钟，MainWindow.cs中是主要的代码， 踩过的坑： 1.第一次运行报错，需要右击解决方案，在属性中设置项目运行顺序，是运行当前项目还是第一个还是某一个指定项目，得设置好 2.桌面时钟代码编写成功，但是报错xxx属性没有设置或者没有配置，直接删了因为看了这个代码后发现根本没用上这个属性或者说这个属性对于项目的目标没有太大影响，先删除
    Signed-off-by: AIminminAI <1317957931@qq.com>

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading; //代表多线程相关的命名空间
+
+namespace WpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            //定义间隔多长时间会触发这个事件,相当于给这个定时器一个属性值
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1); //用多长时间的秒数来生成TimeSpan，这个时间间隔或者说时间跨度
+            //定义事件处理器
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            //启动事件
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object? sender, EventArgs e)
+        {
+            this.timerTextBox.Text = DateTime.Now.ToString();
+        }
+    }
+}

235e6322240c284e526c941704d3c45813ea8732
AIminminAI <1317957931@qq.com>
2023-07-23 15:36:44 +0000

    咱们写的Human类有俩属性，一个string类型的Name，一个Human类型的Child，而WPFDemo01中只把String类型的Name通过点击Button跳转出来，而如果想直接按照Name的方式通过点击按钮方式跳转出来Human类型的Child，按照WPFDemo01的方式是不行的，所以，WPFDemo02对代码进行了更新，可以对比看，差异 咱们写的Human类有俩属性，一个string类型的Name，一个Human类型的Child，而WPFDemo01中只把String类型的Name通过点击Button跳转出来，而如果想直接按照Name的方式通过点击按钮方式跳转出来Human类型的Child，按照WPFDemo01的方式是不行的，所以，WPFDemo02对代码进行了更新，可以对比看，差异
    Signed-off-by: AIminminAI <1317957931@qq.com>

---
+++ WPFDemo02/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShowNameWhenClickButtonFromWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Human h = this.FindResource("human") as Human;
+            if(h != null)
+            {
+                MessageBox.Show(h.Name);
+                Thread.Sleep(1000);
+                MessageBox.Show(h.Child.Name);
+            }
+        }
+    }
+
+    /**
+     * [TypeConverter(typeof(NameToHumanTypeConverter))]代表，将NameToHumanTypeConverter这个类以特性的形式附加到Human这个类上面
+     */
+    [TypeConverter(typeof(NameToHumanTypeConverter))]
+    public class Human
+    {
+        public string Name { get; set; }
+        public Human Child { get; set; }
+
+    }
+
+    public class NameToHumanTypeConverter : TypeConverter
+    {
+        public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+        {
+            string name = value.ToString();
+            Human child = new Human();
+            child.Name = name;
+            return child;
+        }
+    }
+}

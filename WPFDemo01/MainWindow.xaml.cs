47240b3e4e0930f652ba89cda37a79116c1538ae
AIminminAI <1317957931@qq.com>
2023-07-23 12:18:28 +0000

    写一个按钮，然后点击这个按钮，弹出来一个弹窗，弹窗里面是咱们自己定义的Name属性的内容 第一个例子，写一个按钮，然后点击这个按钮，弹出来一个弹窗，弹窗里面是咱们自己定义的Name属性的内容
    Signed-off-by: AIminminAI <1317957931@qq.com>

---
+++ WPFDemo01/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShowNameWhenClickButtonFromWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Human h = this.FindResource("human") as Human;
+            if(h != null)
+            {
+                MessageBox.Show(h.Name);
+            }
+        }
+    }
+
+    public class Human
+    {
+        public string Name { get; set; }
+        public Human Child { get; set; }
+
+    }
+}

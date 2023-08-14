62012ecf5a93382eb0c57b86c21f056e0e0b0d54
AIminminAI <1317957931@qq.com>
2023-08-14 15:09:44 +0000

    嵌套的事件实现，不好，难以排查错误和后期维护 嵌套的事件实现，不好，难以排查错误和后期维护
    Signed-off-by: AIminminAI <1317957931@qq.com>

---
+++ 包含一个事件嵌套坏的实现、自己的一般实现，杰哥的好代码实现的一个业务需求/嵌套的事件实现，不好，难以排查错误和后期维护/StepOne.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfExercise
+{
+    /// <summary>
+    /// StepOne.xaml 的交互逻辑
+    /// </summary>
+    public partial class StepOne : Window
+    {
+        public StepOne()
+        {
+            InitializeComponent();
+            this.average.Click += this.HbClickedEvent;
+        }
+
+
+        private void JumpToStepTwo(object sender, RoutedEventArgs e)
+        {
+            StepTwo stepTwo = new StepTwo();
+            stepTwo.Show();
+        }
+
+        private void HbClickedEvent(object sender, RoutedEventArgs e)
+        {
+            this.HuBox.Text = "8.2,first wpf";
+        }
+    }
+}
---
+++ 包含一个事件嵌套坏的实现、自己的一般实现，杰哥的好代码实现的一个业务需求/嵌套的事件实现，不好，难以排查错误和后期维护/StepTwo.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfExercise
+{
+    /// <summary>
+    /// StepTwo.xaml 的交互逻辑
+    /// </summary>
+    public partial class StepTwo : Window
+    {
+        public StepTwo()
+        {
+            InitializeComponent();
+        }
+
+        private void JumpToStepThree(object sender, RoutedEventArgs e)
+        {
+            StepThree stepThree = new StepThree();
+            stepThree.Show();
+        }
+    }
+}

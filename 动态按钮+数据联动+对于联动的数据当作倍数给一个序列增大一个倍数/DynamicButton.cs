b4fe2fc37c5114a6e3aa1d564eedfcd649ac9f27
AIminminAI <1317957931@qq.com>
2023-08-14 14:51:31 +0000

    动态按钮+数据联动+对于联动的数据当作倍数给一个序列增大一个倍数 动态按钮+数据联动+对于联动的数据当作倍数给一个序列增大一个倍数
    Signed-off-by: AIminminAI <1317957931@qq.com>

---
+++ 动态按钮+数据联动+对于联动的数据当作倍数给一个序列增大一个倍数/DynamicButton.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Controls;
+
+namespace WpfExercise03
+{
+    /// <summary>
+    /// 动态按钮模块
+    /// </summary>
+    public class DynamicButton
+    {
+        public DynamicButton(Button button, double right, double bottom)
+        {
+            _button = button;
+            _right = right;
+            _bottom = bottom;
+            timer = new Timer(50);
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        public void SetEnable(bool flag)
+        {
+            if (flag)
+            {
+                //Canvas.SetLeft(_button, 0);
+                //Canvas.SetTop(_button, 0);
+                timer.Start();
+            }
+            else
+            {
+                timer.Stop();
+                //Canvas.SetLeft(_button, 0);
+                //Canvas.SetTop(_button, 0);
+            }
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _button.Dispatcher.Invoke(() =>
+            {
+                Canvas.SetLeft(_button, Canvas.GetLeft(_button) + speedHorizontal);
+                Canvas.SetTop(_button, Canvas.GetTop(_button) + speedVert);
+                //speedHorizontal = Canvas.GetLeft(_button) > _right ? -5 : 5;
+                //speedVert = Canvas.GetTop(_button) > _bottom ? -5 : 5;
+
+                if (Canvas.GetTop(_button) > _bottom && speedVert > 0)
+                {
+                    speedVert *= -1;
+                }
+                else if(Canvas.GetTop(_button) < 0 && speedVert < 0)
+                {
+                    speedVert *= -1;
+                }
+
+                if (Canvas.GetLeft(_button) > _right && speedHorizontal > 0)
+                {
+                    speedHorizontal *= -1;
+                }
+                else if (Canvas.GetLeft(_button) < 0 && speedHorizontal < 0)
+                {
+                    speedHorizontal *= -1;
+                }
+            });
+        }
+
+
+        #region privatemembers
+
+        private readonly Button _button;
+        private readonly double _right;
+        private readonly double _bottom;
+        private readonly Timer timer;
+
+        private double speedVert = 5f;
+        private double speedHorizontal = 5f;
+        #endregion
+    }
+}
---
+++ 动态按钮+数据联动+对于联动的数据当作倍数给一个序列增大一个倍数/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfExercise03
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            inside_canvas.Loaded += (s, e) =>
+              {
+                  dynamicButton = new DynamicButton(btn_222, inside_canvas.ActualWidth - btn_222.ActualWidth, inside_canvas.ActualHeight - btn_222.ActualHeight);
+              };
+
+            txt_111.TextChanged += Txt_111_TextChanged;
+        }
+
+        private void Txt_111_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            btn_222.Content = (sender as TextBox).Text;
+        }
+
+        private void Button_click(object sender, RoutedEventArgs e)
+        {
+            var a = new List<int> { 1, 5, 7, 9, 7 };
+            var b = (from c in a
+                     let f = c * Convert.ToInt32((sender as Button).Content)
+                     select f).OrderBy(x => x).GroupBy(x => x).Select(x => x.FirstOrDefault());
+            MessageBox.Show($"[{string.Concat(b.Select(x => $"{x},")).TrimEnd(',')}]");
+        }
+
+
+        DynamicButton dynamicButton;
+        bool flag = false;
+
+        private void Btn_start_Click(object sender, MouseButtonEventArgs e)
+        {
+            flag = !flag;
+            dynamicButton?.SetEnable(flag);
+        }
+    }
+}

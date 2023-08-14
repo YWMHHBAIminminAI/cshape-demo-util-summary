04172189ba59e6152cc90c474235643776a92f3d
AIminminAI <1317957931@qq.com>
2023-08-14 15:22:20 +0000

    两个按钮控制一个Label，各按各的并且各自显示各自内容 两个按钮控制一个Label，各按各的并且各自显示各自内容
    Signed-off-by: AIminminAI <1317957931@qq.com>

---
+++ 两个按钮控制一个Label，各按各的并且各自显示各自内容/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace WindowsFormsTwoButtonEventDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOne = new System.Windows.Forms.Button();
+            this.buttonTwo = new System.Windows.Forms.Button();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonOne
+            // 
+            this.buttonOne.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.buttonOne.Location = new System.Drawing.Point(3, 287);
+            this.buttonOne.Name = "buttonOne";
+            this.buttonOne.Size = new System.Drawing.Size(237, 73);
+            this.buttonOne.TabIndex = 0;
+            this.buttonOne.Text = "buttonOne";
+            this.buttonOne.UseVisualStyleBackColor = false;
+            this.buttonOne.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // buttonTwo
+            // 
+            this.buttonTwo.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.buttonTwo.Location = new System.Drawing.Point(275, 287);
+            this.buttonTwo.Name = "buttonTwo";
+            this.buttonTwo.Size = new System.Drawing.Size(235, 73);
+            this.buttonTwo.TabIndex = 1;
+            this.buttonTwo.Text = "buttonTwo";
+            this.buttonTwo.UseVisualStyleBackColor = false;
+            this.buttonTwo.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(13, 188);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(41, 12);
+            this.messageLabel.TabIndex = 2;
+            this.messageLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.buttonTwo);
+            this.Controls.Add(this.buttonOne);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOne;
+        private System.Windows.Forms.Button buttonTwo;
+        private System.Windows.Forms.Label messageLabel;
+    }
+}
+
---
+++ 两个按钮控制一个Label，各按各的并且各自显示各自内容/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTwoButtonEventDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 在Form类中创建一个名为Button_Click的事件处理程序
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if(((Button)sender).Name == "buttonOne")
+            {
+                messageLabel.Text = "信息：Button One was Clicked";
+            }
+            else
+            {
+                messageLabel.Text = "信息：Button Two was Clicked";
+            }
+            
+        }
+    }
+}

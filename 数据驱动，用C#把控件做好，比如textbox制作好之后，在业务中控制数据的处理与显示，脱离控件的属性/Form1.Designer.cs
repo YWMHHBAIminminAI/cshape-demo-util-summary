6150d09326a480cbc9eec06cd57ef58444bff3c5
AIminminAI <1317957931@qq.com>
2023-09-01 14:52:43 +0000

    数据驱动，用C#把控件做好，比如textbox制作好之后，在业务中控制数据的处理与显示，脱离控件的属性
    Signed-off-by: AIminminAI <1317957931@qq.com>

---
+++ 数据驱动，用C#把控件做好，比如textbox制作好之后，在业务中控制数据的处理与显示，脱离控件的属性/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_Name = new System.Windows.Forms.TextBox();
+            this.tb_Desc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_Name
+            // 
+            this.tb_Name.Location = new System.Drawing.Point(249, 103);
+            this.tb_Name.Name = "tb_Name";
+            this.tb_Name.Size = new System.Drawing.Size(322, 21);
+            this.tb_Name.TabIndex = 0;
+            // 
+            // tb_Desc
+            // 
+            this.tb_Desc.Location = new System.Drawing.Point(249, 187);
+            this.tb_Desc.Name = "tb_Desc";
+            this.tb_Desc.Size = new System.Drawing.Size(322, 21);
+            this.tb_Desc.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "名称";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "描述";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_Desc);
+            this.Controls.Add(this.tb_Name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_Name;
+        private System.Windows.Forms.TextBox tb_Desc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 数据驱动，用C#把控件做好，比如textbox制作好之后，在业务中控制数据的处理与显示，脱离控件的属性/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var form = new Form1();
+
+            form.InitData(new MyData
+            {
+                Name = "aaa",
+                Descriptions = "我的描述"
+            });
+
+    //        form.UpdateData("Name", "张三");
+
+    //        timer.Interval = 1000;
+    //        var index = 0;
+    //        timer.Tick += (s, e) =>
+    //        {
+    //            var key = index % 2 == 0 ? "Name" : "Desc";
+    //            form.UpdateData(key, index.ToString());
+    //            index++;
+    //        };
+    //        timer.Start();
+
+
+            Application.Run(form);
+        }
+
+    //    static Timer timer = new Timer();
+    }
+}

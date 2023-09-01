a785f1113164db1d97868052e94fd4a26902b097
AIminminAI <1317957931@qq.com>
2023-09-01 15:07:22 +0000

    在数据驱动基础上，加上了业务输入的时间数据，自动在控件内自增，动态变化
    Signed-off-by: AIminminAI <1317957931@qq.com>

---
+++ 在数据驱动基础上，加上了业务输入的时间数据，自动在控件内自增，动态变化/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void InitData(MyData data)
+        {
+            tb_Name.Text = data.Name;
+            tb_Desc.Text = data.Descriptions;
+        }
+
+        public void UpdateData(string dataType,string data)
+        {
+            switch(dataType)
+            {
+                case "Name":
+                    tb_Name.Text = data;
+                    break;
+                case "Desc":
+                    tb_Desc.Text = data;
+                    break;
+            }
+        }
+    }
+}

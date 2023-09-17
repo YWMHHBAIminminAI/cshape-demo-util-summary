17f8088412bbeca8788328049f0570c97a8d5f89
AIminminAI <1317957931@qq.com>
2023-09-17 03:55:20 +0000

    Version01_两个输入框输入加数和被加数，第三个框显示和 Version01_两个输入框输入加数和被加数，第三个框显示和
    Signed-off-by: AIminminAI <1317957931@qq.com>

---
+++ MVVM/Version01_两个输入框输入加数和被加数，第三个框显示和/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace AddNotUseMVVM
+{
+    public class Class1
+    {
+    }
+}
---
+++ MVVM/Version01_两个输入框输入加数和被加数，第三个框显示和/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AddNotUseMVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 实现 通过add按钮，实现两个文本框中的数字相加
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void addButton_click(object sender, RoutedEventArgs e)
+        {
+            //实战尽量不要用parse方法这样写
+            double addVal1 = double.Parse(tb1.Text);
+            double addVal2 = double.Parse(tb2.Text);
+            double res = addVal1 + addVal2;
+            this.tb3.Text = res.ToString();
+        }
+
+        /// <summary>
+        /// 实现 通过save按钮，实现保存两个文本框中的数字相加结果
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.ShowDialog();
+        }
+    }
+}

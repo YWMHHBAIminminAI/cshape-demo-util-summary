8c120ec5c9a63e1b4e54b9d8b11ce194fa9c9c6d
AIminminAI <1317957931@qq.com>
2023-10-05 11:03:38 +0000

    version02_添加可拖动控件 version02_添加可拖动控件
    Signed-off-by: AIminminAI <1317957931@qq.com>

---
+++ MVVM/Version02_/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AddNotUseMVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void addButton_click(object sender, RoutedEventArgs e)
+        {
+            this.slider3.Value = this.slider2.Value + this.slider1.Value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void save_click(object sender, RoutedEventArgs e)
+        {
+            new SaveFileDialog().ShowDialog();
+        }
+    }
+}
